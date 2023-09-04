70c4dbef8bc2b590f2ebd6d4c22bd3a7e3f865cc
patrickwide <patrickwide254@gmail.com>
2023-09-05 02:49:03 +0300

    Add readme

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,17 +1,30 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    # Class attribute
+    """Represent a rectangle.
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
+
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        # Initialize width and height using the properties
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
-        # Increment the number of instances
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -24,6 +37,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -35,46 +49,61 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
-
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """Return the Rectangle with the greater area.
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
-        area1 = rect_1.area()
-        area2 = rect_2.area()
-        
-        if area1 >= area2:
-            return rect_1
-        else:
-            return rect_2
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        """Return a new Rectangle with width and height equal to size.
+        Args:
+            size (int): The width and height of the new Rectangle.
+        """
+        return (cls(size, size))
 
-# Example usage
-if __name__ == "__main__":
-    my_square = Rectangle.square(5)
-    print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-    print(my_square)
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
+
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,72 @@
+Certainly! Here's a simplified README for the provided Python tasks:
+
+# Python - More Classes and Objects
+
+This set of Python tasks is designed to help you practice working with classes and objects. Each task builds upon the previous one, adding more features and functionality to a `Rectangle` class.
+
+## Task 0: 0-rectangle.py
+
+- Create an empty class called `Rectangle` that represents a rectangle.
+
+## Task 1: 1-rectangle.py
+
+- Add private instance attributes `width` and `height`.
+- Implement getters and setters for `width` and `height`.
+- Ensure that `width` and `height` are integers and not negative.
+
+## Task 2: 2-rectangle.py
+
+- Add methods `area()` and `perimeter()` to calculate the area and perimeter of the rectangle.
+- Make sure the perimeter is 0 if either `width` or `height` is 0.
+
+## Task 3: 3-rectangle.py
+
+- Implement `__str__` and `__repr__` methods to print and represent the rectangle.
+- Use '#' to represent the rectangle in strings.
+- Ensure that an empty string is returned if `width` or `height` is 0.
+
+## Task 4: 4-rectangle.py
+
+- Add a class attribute `number_of_instances` to keep track of the number of instances.
+- Increment it during each instantiation and decrement during deletion.
+
+## Task 5: 5-rectangle.py
+
+- Add a class attribute `print_symbol` to customize the symbol used for string representation.
+- Update `__str__` and `__repr__` to use this symbol.
+
+## Task 6: 6-rectangle.py
+
+- Add a static method `bigger_or_equal` to compare two rectangles based on area.
+- Raise TypeError if the input is not a `Rectangle` instance.
+- Return the rectangle with the larger area.
+
+## Task 7: 7-rectangle.py
+
+- Add a class method `square` to create a square `Rectangle` with the same width and height.
+- Use this method to create square instances.
+
+## Task 8: 8-rectangle.py
+
+- Enhance the `bigger_or_equal` method to return the first rectangle if the areas are equal.
+
+## Task 9: 9-rectangle.py
+
+- Complete the class by adding all previous functionality.
+- Improve the class attributes and methods.
+
+## Task 10: 101-nqueens.py
+
+- Solve the N queens puzzle using backtracking.
+- Print all possible solutions for a given N on the chessboard.
+
+# Usage:
+
+```
+python 101-nqueens.py N
+```
+
+- N must be an integer >= 4.
+- The program prints solutions to the N queens problem, one per line.
+
+Enjoy coding and practicing your Python skills!
